ee625730592a859569831ee912dd2ffe50955a8c
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-10-08 09:01:14 -0300

    feat: create models users and projects

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,27 +21,4 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
